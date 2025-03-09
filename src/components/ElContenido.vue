299dc8b0887172ce5cb1bccd0248c13ff2ac722c
Nicolas Jimenez <nicolasdanieljimenez@hotmail.com>
2025-03-09 20:15:34 -0300

    Componentes creados (?

---
+++ src/components/ElContenido.vue
@@ -0,0 +1,23 @@
+<template>
+  <span :class="'contenido ' + contenidoClass">
+    <slot name="contenido" />
+  </span>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  contenidoClass: {
+    type: String,
+    default: '',
+  },
+})
+</script>
+
+<style scoped>
+.contenido {
+  font-size: 18px;
+  margin-top: 10px;
+}
+</style>
---
+++ src/components/ElTitulo.vue
@@ -0,0 +1,22 @@
+<template>
+  <p :class="'titulo ' + tituloClass">{{ titulo }}</p>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  titulo: String,
+  tituloClass: {
+    type: String,
+  },
+})
+</script>
+
+<style scoped>
+.titulo {
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 1rem;
+}
+</style>
