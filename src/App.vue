a5644658fbc73924125e7d530a1fc0f5fe4e359a
Nicolas Jimenez <nicolasdanieljimenez@hotmail.com>
2025-03-09 18:11:59 -0300

    Añadida vista Variables

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/fundamentos/programacion/variables">Variables</RouterLink>
       </nav>
     </div>
   </header>
---
+++ src/views/FundamentosProgramacion/VariablesView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Fundamentos de Programación: Variables</h1>
+    <input v-model="variable" placeholder="Escribe algo..." />
+    <p>Valor de la variable: {{ variable }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const variable = ref('')
+</script>
+
+<style scoped>
+h1 {
+  color: #333;
+}
+
+input {
+  margin-top: 10px;
+  padding: 5px;
+  font-size: 16px;
+}
+
+p {
+  margin-top: 10px;
+  font-size: 18px;
+}
+</style>
